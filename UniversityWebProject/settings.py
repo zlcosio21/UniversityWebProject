6468442d89135bf74e926cc1ba0c39f84b47dd51
Sebastian <sebas60621@gmail.com>
2023-09-03 21:40:27 -0500

    Configurando BBDD a Mysql

--- UniversityWebProject/settings.py
+++ UniversityWebProject/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'UniversityWebProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'universidad',
+        'USER': 'root',
+        'PASSWORD': 'minato21',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
