4f3bb4f11ea7cd943f1735b005e085b3d57b3456
Sebastian <sebas60621@gmail.com>
2023-09-05 00:08:42 -0500

    Estructura basica de creacion de cuenta de usuario

--- registro/views.py
+++ registro/views.py
@@ -1,6 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, Group
+from django.db.utils import IntegrityError
 
 # Create your views here.
 def registro(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        password_confirm = request.POST.get("password_confirm")
+        tipo_usuario = request.POST.get("tipo_usuario")
+
+        if password == password_confirm:
+
+            try:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+
+                if tipo_usuario == "estudiante":
+                    group = Group.objects.get(name="Estudiantes")
+                else:
+                    group = Group.objects.get(name="Profesores")
+
+                user.groups.add(group)
+
+            except IntegrityError:
+                redirect("inicio")    
+
+            return redirect("inicio")
+
+        else:
+            return redirect("inicio")
 
     return render(request, "registro/registro.html")
