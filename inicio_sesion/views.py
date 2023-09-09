1e450bd396ab78a5c477c9a5f164956f96b596aa
Sebastian <sebas60621@gmail.com>
2023-09-08 23:33:39 -0500

    Inicio de sesion del usuario

--- inicio_sesion/views.py
+++ inicio_sesion/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def inicio_sesion(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+
+            return redirect("inicio")
+        
+        else:
+            return redirect("inicio_sesion")
 
     return render(request, "inicio_sesion/inicio_sesion.html")
