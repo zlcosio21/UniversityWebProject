e82fba500b08b4582b3f3aff7052dd54e888f97d
Sebastian <sebas60621@gmail.com>
2023-09-10 15:21:53 -0500

    Consultando las categorias de los post, Estudiante/Profesor

--- UniversityWebApp/views.py
+++ UniversityWebApp/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
-from blog.models import Post
+from blog.models import Post, CategoriaCarrera
 
 # Create your views here.
 def inicio(request):
 
-    posts = Post.objects.all()
+    profesor = CategoriaCarrera.objects.get(id=1)
+    estudiante = CategoriaCarrera.objects.get(id=2)
 
-    return render(request, "UniversityWebApp/inicio.html", {"posts":posts})
+    post_profesor = Post.objects.filter(categorias = profesor)
+    post_estudiante = Post.objects.filter(categorias = estudiante)
+
+    return render(request, "UniversityWebApp/inicio.html", {"post_profesor":post_profesor, "post_estudiante":post_estudiante})
 
 def perfil_usuario(request):
 
