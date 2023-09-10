c187a0f7f1e9bb7ce4cd39507e08462bed0b2488
Sebastian <sebas60621@gmail.com>
2023-09-09 23:21:25 -0500

    Obteniendo los registros creados de los modelos del blog

--- UniversityWebApp/views.py
+++ UniversityWebApp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from blog.models import Post
 
 # Create your views here.
 def inicio(request):
 
-    return render(request, "UniversityWebApp/inicio.html")
+    posts = Post.objects.all()
+
+    return render(request, "UniversityWebApp/inicio.html", {"posts":posts})
 
 def perfil_usuario(request):
 
