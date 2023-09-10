b5625a99e5029eb3c0ab3ee63945d625eaf3d706
Sebastian <sebas60621@gmail.com>
2023-09-09 23:16:47 -0500

    Permitiendo usar imagenes en los registros

--- UniversityWebApp/urls.py
+++ UniversityWebApp/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from UniversityWebApp import views
+from django.conf import settings
+from django.conf.urls.static import static 
 
 urlpatterns = [
     
@@ -24,3 +26,5 @@ urlpatterns = [
     path('perfil_usuario/', views.perfil_usuario, name="perfil_usuario"),
 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- UniversityWebProject/settings.py
+++ UniversityWebProject/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from django.conf import settings
+from django.conf.urls.static import static 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,6 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
