b08776b3bb88146dc8109ecef2e5989041c28514
Sebastian <sebas60621@gmail.com>
2023-09-10 22:57:58 -0500

    Modelos para las carreras e incluyendo en el admin

--- carreras/admin.py
+++ carreras/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from carreras.models import Carreras
 
 # Register your models here.
+admin.site.register(Carreras)
--- carreras/models.py
+++ carreras/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Carreras(models.Model):
+    nombre = models.CharField(unique=True, max_length=50, null=False)
+
+    class meta:
+        verbose_name = 'carrera'
+        verbose_name_plural = 'carreras'
+
+    def __str__(self):
+        return self.nombre
