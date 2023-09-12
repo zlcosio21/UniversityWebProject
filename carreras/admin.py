709215f8aedbfabadbd8b2b9cf2bbca48f04783f
Sebastian <sebas60621@gmail.com>
2023-09-11 23:24:41 -0500

    Creacion del modelo semestre y registrandolo en el panel

--- carreras/admin.py
+++ carreras/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from carreras.models import Carrera
+from carreras.models import Carrera, Semestre
 
 # Register your models here.
 admin.site.register(Carrera)
+admin.site.register(Semestre)
--- carreras/models.py
+++ carreras/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Carrera(models.Model):
@@ -10,3 +12,10 @@ class Carrera(models.Model):
 
     def __str__(self):
         return self.nombre
+    
+class Semestre(models.Model):
+    numero = models.PositiveIntegerField()
+    carrera = models.ForeignKey(Carrera, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.carrera.nombre} - Semestre {self.numero}"
