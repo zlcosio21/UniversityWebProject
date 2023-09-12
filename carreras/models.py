54a44317917e280ad5da371af55a91832d354847
Sebastian <sebas60621@gmail.com>
2023-09-12 15:31:06 -0500

    Creacion modelo salon

--- carreras/models.py
+++ carreras/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User, Group
 
 # Create your models here.
 class Carrera(models.Model):
@@ -20,11 +21,26 @@ class Semestre(models.Model):
     
 class Materia(models.Model):
     nombre = models.CharField(max_length=50, unique=True, null=False)
-    semestre_perteneciente_a  = models.ForeignKey(Semestre, on_delete=models.CASCADE)
+    materia_perteneciente_a  = models.ForeignKey(Semestre, on_delete=models.CASCADE)
     
     class meta:
         verbose_name = 'materia'
         verbose_name_plural = 'materias'
 
     def __str__(self):
-        return f"{self.nombre} - Carrera {self.semestre_perteneciente_a .carrera.nombre} - Semestre {self.semestre_perteneciente_a .numero}"
+        return f"{self.nombre} - Carrera {self.materia_perteneciente_a.carrera.nombre} - Semestre {self.materia_perteneciente_a.numero}"
+    
+
+class Salon(models.Model):
+    nombre = models.CharField(max_length=50, unique=True, null=False)
+    estudiantes = models.ManyToManyField(User, related_name='salones_estudiantes', blank=True, limit_choices_to={'groups__name': 'Estudiante'})
+    profesor = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='salones_profesor', blank=True, limit_choices_to={'groups__name': 'Profesor'})
+    materia = models.ForeignKey(Materia, on_delete=models.CASCADE, null=False, default=1)
+    semestre = models.ForeignKey(Semestre, on_delete=models.CASCADE, null=False)
+
+    class Meta:
+        verbose_name = 'salon'
+        verbose_name_plural = 'salones'
+
+    def __str__(self):
+        return f"Salon {self.nombre} - Materia {self.materia.nombre} - Carrera {self.semestre.carrera.nombre} - Semestre {self.semestre.numero} - Profesor {self.profesor}"
