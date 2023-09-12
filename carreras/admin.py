c39fc54864fe73e145246ebf946c3d1e412e984c
Sebastian <sebas60621@gmail.com>
2023-09-12 15:31:30 -0500

    Registrando el modelo salon al admin

--- carreras/admin.py
+++ carreras/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from carreras.models import Carrera, Semestre, Materia
+from carreras.models import Carrera, Semestre, Materia, Salon
 
 # Register your models here.
 admin.site.register(Carrera)
 admin.site.register(Semestre)
 admin.site.register(Materia)
+admin.site.register(Salon)
