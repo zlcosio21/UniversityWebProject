d92cdb93f9482dff410d13be27fe985f72fae639
Sebastian <sebas60621@gmail.com>
2023-09-13 17:46:45 -0500

    Matricular a una sola carrera de la universidad

--- carreras/views.py
+++ carreras/views.py
@@ -1,15 +1,32 @@
 from django.shortcuts import render, redirect
-from carreras.models import Carrera
+from carreras.models import Carrera, Materia
 
 # Create your views here.
 def matricular_carrera(request, carrera_nombre):
     try:
+
         carrera = Carrera.objects.get(nombre__iexact=carrera_nombre)
+        materias_carrera = Materia.objects.filter(materia_perteneciente_a__carrera=carrera)
 
-        carrera.estudiantes.add(request.user)
-        carrera.save()
+        user = request.user
+        
+        carrera_matriculada = user.carreras_matriculadas.filter(id=carrera.id).exists()
+        ya_esta_matriculado = user.carreras_matriculadas.exists()
+        
+        if carrera_matriculada:
+            carrera.estudiantes.add(user)
+            carrera.save()
 
-        return redirect('perfil_usuario')
+            return render(request, "carreras/carrera.html", {"carrera_nombre": carrera_nombre, "materias_carrera":materias_carrera})
+
+        elif ya_esta_matriculado:
+            return render(request, "carreras/ya_matriculado.html", {"carrera_nombre":carrera_nombre})
+        
+        else:
+            carrera.estudiantes.add(user)
+            carrera.save()
+
+            return render(request, "carreras/carrera.html", {"carrera_nombre": carrera_nombre, "materias_carrera":materias_carrera})
         
     except carrera.DoesNotExist:
         return redirect('inicio')
