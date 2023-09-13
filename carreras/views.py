28f53f6578bfcfa2ea7b2d8447ccc2ca108dfc91
Sebastian <sebas60621@gmail.com>
2023-09-12 22:59:32 -0500

    Vista para matricular a las carreras

--- carreras/views.py
+++ carreras/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from carreras.models import Carrera
 
 # Create your views here.
+def matricular_carrera(request, carrera_nombre):
+    try:
+        carrera = Carrera.objects.get(nombre__iexact=carrera_nombre)
+
+        carrera.estudiantes.add(request.user)
+        carrera.save()
+
+        return redirect('perfil_usuario')
+        
+    except carrera.DoesNotExist:
+        return redirect('inicio')
