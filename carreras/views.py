63398f7cba75c3d9a87ee6e07ae47dd02f002a31
Sebastian <sebas60621@gmail.com>
2023-09-13 23:21:27 -0500

    Matriculando automaticamente al semestre 1

--- carreras/views.py
+++ carreras/views.py
@@ -6,7 +6,9 @@ def matricular_carrera(request, carrera_nombre):
     try:
 
         carrera = Carrera.objects.get(nombre__iexact=carrera_nombre)
-        materias_carrera = Materia.objects.filter(semestre__carrera=carrera)
+        materias_carrera = Materia.objects.filter(semestre__carrera=carrera, semestre__numero=1)
+
+        salones_carrera = Salon.objects.filter(semestre__carrera=carrera, semestre__numero=1)
 
         user = request.user
         
@@ -14,10 +16,8 @@ def matricular_carrera(request, carrera_nombre):
         ya_esta_matriculado = user.carreras_matriculadas.exists()
         
         if carrera_matriculada:
-            carrera.estudiantes.add(user)
-            carrera.save()
-
-            return render(request, "carreras/carrera.html", {"carrera_nombre": carrera_nombre, "materias_carrera":materias_carrera})
+            
+            return render(request, "carreras/carrera.html", {"carrera_nombre": carrera_nombre, "materias_carrera":materias_carrera, "salones_carrera":salones_carrera})
 
         elif ya_esta_matriculado:
             return render(request, "carreras/ya_matriculado.html", {"carrera_nombre":carrera_nombre})
@@ -26,7 +26,7 @@ def matricular_carrera(request, carrera_nombre):
             carrera.estudiantes.add(user)
             carrera.save()
 
-            return render(request, "carreras/carrera.html", {"carrera_nombre": carrera_nombre, "materias_carrera":materias_carrera})
+            return render(request, "carreras/carrera.html", {"carrera_nombre": carrera_nombre, "materias_carrera":materias_carrera, "salones_carrera":salones_carrera})
         
     except carrera.DoesNotExist:
         return redirect('inicio')
