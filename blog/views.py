07b2278c537d649fb0a24f277ea2758beb0f64b8
Sebastian <sebas60621@gmail.com>
2023-09-14 21:53:31 -0500

    Corrigiendo nombre de modelo de App blog

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from blog.models import Post, CategoriaCarrera
+from blog.models import Post, TipoUsuario
 
 # Create your views here.
 def categoria(request, categoria_id):
     
-    categoria = CategoriaCarrera.objects.get(id = categoria_id)
+    categoria = TipoUsuario.objects.get(id = categoria_id)
     posts = Post.objects.filter(categorias = categoria)
 
     return render(request, "blog/categoria.html", {"categoria":categoria, "posts":posts})
