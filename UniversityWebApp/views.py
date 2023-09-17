2c9e8955cfcf8350d88180a81cbd3113d84c67f1
Sebastian <sebas60621@gmail.com>
2023-09-17 13:13:12 -0500

    Eliminando lineas innecesarias y redireccionando al registro

--- UniversityWebApp/views.py
+++ UniversityWebApp/views.py
@@ -1,22 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from blog.models import Post, TipoUsuario
-from carreras.models import Carrera
 
 # Create your views here.
 def inicio(request):
-    carreras = Carrera.objects.all()
-    lista_carreras = []
-
-    for carrera in carreras:
-        lista_carreras.append(carrera.nombre)
 
     estudiante = TipoUsuario.objects.get(id=1)
     profesor = TipoUsuario.objects.get(id=2)
     
     post_profesor = Post.objects.filter(categorias = profesor)
     post_estudiante = Post.objects.filter(categorias = estudiante)
+    
+    user = request.user
 
-    return render(request, "UniversityWebApp/inicio.html", {"post_profesor":post_profesor, "post_estudiante":post_estudiante, "lista_carreras":lista_carreras})
+    if not user.is_authenticated:
+        return redirect("registro")
+
+    return render(request, "UniversityWebApp/inicio.html", {"post_profesor":post_profesor, "post_estudiante":post_estudiante})
 
 def perfil_usuario(request):
 
