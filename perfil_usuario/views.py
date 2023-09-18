5c6e7b4f54af0b1ac5491e9caf5084b2179279cc
Sebastian <sebas60621@gmail.com>
2023-09-17 21:43:50 -0500

    Permitiendo cambiar de contraseña e incluyendo mensajes de error

--- perfil_usuario/views.py
+++ perfil_usuario/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import login
 
 # Create your views here.
 def perfil_usuario(request):
@@ -14,5 +16,31 @@ def editar_perfil(request):
     return render(request, "perfil_usuario/editar_perfil.html", {"active_tab":"editar_perfil"})
 
 def cambiar_password(request):
+
+    if request.method == "POST":
+        password_actual = request.POST.get("password")
+        password_actual_valida = request.user.check_password(password_actual)
+
+        nueva_password = request.POST.get("new_password")
+        nueva_password_confirm = request.POST.get("new_password_confirm")
+
+        user = request.user
+
+        if len(nueva_password) < 8 and len(nueva_password_confirm) < 8:
+            messages.error(request, "La contraseña debe tener minimo 8 caracteres", extra_tags="error_characters")
+            
+        if nueva_password != nueva_password_confirm:
+            messages.error(request, "Las contraseñas deben ser iguales. Ingrese nuevamente", extra_tags="password_error")
+        
+        if not password_actual_valida:
+            messages.error(request, "La contraña actual es incorrecta, ingrese nuevamente", extra_tags="password_novalida")
+        
+        if password_actual_valida and nueva_password == nueva_password_confirm:   
+            user.set_password(nueva_password)
+            user.save()
+
+            login(request, user)
+
+            return redirect("inicio")
 
     return render(request, "perfil_usuario/cambiar_password.html")
