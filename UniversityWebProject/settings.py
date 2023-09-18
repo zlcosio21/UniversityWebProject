7df21f055564249b431cdf16b6d846785f56936e
Sebastian <sebas60621@gmail.com>
2023-09-17 22:01:23 -0500

    Url predeterminada cuando el usuario no esta logeado

--- UniversityWebProject/settings.py
+++ UniversityWebProject/settings.py
@@ -136,3 +136,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# @Login required
+
+LOGIN_URL = '/inicio_sesion/'
