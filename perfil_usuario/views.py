59d23c927ebcdd697695ba00025a1348ea9d54ea
Sebastian <sebas60621@gmail.com>
2023-09-23 20:41:03 -0500

    Usando validaciones de errores y validacion para cambiar la password

--- perfil_usuario/views.py
+++ perfil_usuario/views.py
@@ -58,23 +58,19 @@ def cambiar_password(request):
 
     if request.method == "POST":
         password_actual = request.POST.get("password")
-        password_actual_valida = request.user.check_password(password_actual)
 
         nueva_password = request.POST.get("new_password")
         nueva_password_confirm = request.POST.get("new_password_confirm")
 
         user = request.user
 
-        if len(nueva_password) < 8 and len(nueva_password_confirm) < 8:
-            messages.error(request, "La contraseña debe tener minimo 8 caracteres", extra_tags="error_characters")
-            
-        if nueva_password != nueva_password_confirm:
-            messages.error(request, "Las contraseñas deben ser iguales. Ingrese nuevamente", extra_tags="password_error")
+        characters_error(request, nueva_password, nueva_password_confirm)
+        password_novalida(request, password_actual)
+        equals_error(request, nueva_password, nueva_password_confirm)
+
+        password_actual_valida = request.user.check_password(password_actual)
         
-        if not password_actual_valida:
-            messages.error(request, "La contraña actual es incorrecta, ingrese nuevamente", extra_tags="password_novalida")
-        
-        if password_actual_valida and nueva_password == nueva_password_confirm:   
+        if password_actual_valida and (nueva_password == nueva_password_confirm and len(nueva_password) >= 8 and len(nueva_password_confirm) >= 8):   
             user.set_password(nueva_password)
             user.save()
 
