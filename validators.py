c6fe098bd47f47d0f0d2170386f3fc57a5294df3
Sebastian <sebas60621@gmail.com>
2023-09-23 20:19:54 -0500

    Corrigiendo funcionamiento de los mensajes de error

--- validators.py
+++ validators.py
@@ -2,14 +2,16 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 
 def characters_error(request, *args):
-    if len(args) < 8:
-        messages.error(request, "Debe contener minimo 8 caracteres", extra_tags="username_error")
+    for var in args:
+        if len(var) < 8:
+            messages.error(request, "Debe contener minimo 8 caracteres", extra_tags="characters_error")
 
 def username_existe(request, new_username):
     user = request.user
-    User.objects.filter(username=new_username).exclude(pk=user.pk).exists()
+    existe = User.objects.filter(username=new_username).exclude(pk=user.pk).exists()
 
-    return messages.error(request, "El nuevo nombre de usuario ya está en uso.", extra_tags="username_existe_error")
+    if existe:
+        return messages.error(request, "El nuevo nombre de usuario ya está en uso.", extra_tags="username_existe_error")
 
 def password_novalida(request, password_actual):
     user = request.user
@@ -17,3 +19,8 @@ def password_novalida(request, password_actual):
     password_valida = user.check_password(password_actual)
     if not password_valida:
         return messages.error(request, "La contraña actual es incorrecta, ingrese nuevamente", extra_tags="password_novalida")
+    
+def equals_error(request, password, password_confirm):
+
+    if password != password_confirm:
+        return messages.error(request, "Las contraseñas deben ser iguales. Ingrese nuevamente", extra_tags="password_error")
