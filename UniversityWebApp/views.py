f59c749b778513561bcd93d6705298eef83f439f
Sebastian <sebas60621@gmail.com>
2023-09-27 20:59:33 -0500

    Crear automaticamente el tipo de usuario

--- UniversityWebApp/views.py
+++ UniversityWebApp/views.py
@@ -4,11 +4,14 @@ from blog.models import Post, TipoUsuario
 # Create your views here.
 def inicio(request):
 
-    estudiante = TipoUsuario.objects.get(id=1)
-    profesor = TipoUsuario.objects.get(id=2)
+    tipo_usuario_estudiante, created = TipoUsuario.objects.get_or_create(nombre = "Estudiante")
+    tipo_usuario_profesor, created = TipoUsuario.objects.get_or_create(nombre = "Profesor")
+
+    tipo_usuario_estudiante = TipoUsuario.objects.get(nombre = "Estudiante")
+    tipo_usuario_profesor = TipoUsuario.objects.get(nombre = "Profesor")
     
-    post_profesor = Post.objects.filter(categorias = profesor)
-    post_estudiante = Post.objects.filter(categorias = estudiante)
+    post_estudiante = Post.objects.filter(categoria = tipo_usuario_estudiante)
+    post_profesor = Post.objects.filter(categoria = tipo_usuario_profesor)
     
     user = request.user
 
