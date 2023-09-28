228216084efce0aa96b0441d6a105038d8bdba2b
Sebastian <sebas60621@gmail.com>
2023-09-27 19:59:10 -0500

    Corrigiendo created y updated y su metodo __str__

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,10 @@
 from django.db import models
-from carreras.models import Carrera
 
 # Create your models here.
 class TipoUsuario(models.Model):
     nombre = models.CharField(max_length=50)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
-
-    class meta:
-        verbose_name = 'tipo_usuario'
-        verbose_name_plural = 'tipo_usuario'
 
     def __str__(self):
         return self.nombre
@@ -18,16 +13,13 @@ class Post(models.Model):
     titulo = models.CharField(max_length=50)
     contenido = models.CharField(max_length=50)
     imagen = models.ImageField(upload_to='blog', null=True, blank=True)
-
-    categorias = models.ManyToManyField(TipoUsuario)
-    carrera = models.ForeignKey(Carrera, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(TipoUsuario, on_delete=models.CASCADE)
 
     created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now_add=True)
-
-    class meta:
-        verbose_name = 'post'
-        verbose_name_plural = 'posts'
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.titulo
+        if self.categoria.nombre == "Estudiante":
+            return f"Matricular carrera {self.titulo}"
+        else:
+            return f"Especializar materia {self.titulo}"
