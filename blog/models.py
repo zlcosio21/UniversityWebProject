aeeaf312fec008152a05468f04d8ebffc1a55d32
Sebastian <sebas60621@gmail.com>
2023-09-30 22:39:34 -0500

    Corrigiendo modelo Post de la App blog

--- blog/models.py
+++ blog/models.py
@@ -12,15 +12,15 @@ class TipoUsuario(models.Model):
     
 class Post(models.Model):
     titulo = models.CharField(max_length=50)
-    contenido = models.CharField(max_length=50)
+    contenido = models.CharField(max_length=50, null=True)
     imagen = models.ImageField(upload_to='blog', null=True, blank=True)
-    categoria = models.ForeignKey(TipoUsuario, on_delete=models.CASCADE)
+    tipo_usuario = models.ForeignKey(TipoUsuario, on_delete=models.CASCADE)
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        if self.categoria.nombre == "Estudiante":
-            return f"Matricular carrera {self.titulo}"
+        if self.tipo_usuario.nombre == "Estudiante":
+            return f"Matricular carrera {self.titulo.capitalize()}"
         else:
-            return f"Especializar materia {self.titulo}"
+            return f"Especializar materia {self.titulo.capitalize()}"
