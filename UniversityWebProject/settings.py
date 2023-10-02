e843565e88793815c8e6c0f1120a54817e57337e
Sebastian <sebas60621@gmail.com>
2023-10-01 21:44:29 -0500

    Allowed host vercel

--- UniversityWebProject/settings.py
+++ UniversityWebProject/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = PASSWORD_DJANGO
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
